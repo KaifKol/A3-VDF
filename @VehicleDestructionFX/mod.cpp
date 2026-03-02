7764e9225878e11b86323bd2ceaa035971092364
KaifKol <komrad02kb2020@gmail.com>
2026-03-03 00:20:54 +0200

    init

---
+++ @VehicleDestructionFX/mod.cpp
@@ -0,0 +1,7 @@
+name = "Vehicle Destruction FX";
+author = "KaifKol";
+logo = "images\logo1.paa";
+picture = "images\picture.jpg";
+actionName = "GitHub";
+action = "https://github.com/KaifKol";
+overview = "This mod adds the ability to burn vehicles that the ACE mod doesn't allow.";
